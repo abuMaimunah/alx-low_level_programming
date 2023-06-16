eaf90ce08e74a99dbdb48f1805140b4c0e3d989a
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-16 12:40:02 -0700

    yo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * #includ (stdin.h) - is a preprocessor directives
+ * #include <stdio.h> - it is a preprocessor directives
  */
 
 /**
-  * This is my main function
-  * It returns 0
+  * main - entry point
+  * return - 0 for success
   */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/** return 0 signifies succes **/
 return (0);
 }
