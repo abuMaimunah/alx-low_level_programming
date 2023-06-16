db1b3eb88583415bfff4ee08f7913ad79e17d000
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-16 13:46:36 -0700

    yo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  * main - This is the entry point
-  * Return: 0 is returned for success
-  */
+ * main - This is the entry point
+ * Return: 0 is returned for success
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
