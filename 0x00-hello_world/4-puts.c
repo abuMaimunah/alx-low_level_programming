5a748346a9a4661e5505ac269795c47f27352b2a
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-16 11:37:25 -0700

    yo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ ** A program using puts() funnction
+**/
+/** this is the main function with return tupe of int **/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+/** return 0 signifies succes **/
+return (0);
+}
