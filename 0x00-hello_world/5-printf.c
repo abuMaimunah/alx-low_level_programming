033ed414f0d769cb3f571a7dd90dcfee76df34ba
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-16 12:58:08 -0700

    na wa for betty style woo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the enty point of the program
+ * Return: 0 is returned to indicate succcess
+ */
+int main(void)
+{
+	printf("%d", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
