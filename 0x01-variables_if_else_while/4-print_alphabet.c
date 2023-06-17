403105d0513a100cc2af97f633275ad0258f8de9
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-17 04:43:02 -0700

    yo

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 is returned
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+}
