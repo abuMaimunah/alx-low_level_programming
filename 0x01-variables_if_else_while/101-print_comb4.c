03b59d5439d2ee03572bef7fc6d754bb1b520145
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-19 14:44:52 -0700

    yo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,15 +9,16 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = '0'; i <= 7; i++)
+	for (i = '0'; i <= '7'; i++)
 	{
-		for (j = i + 1; j <= 8; j++)
+		for (j = i + 1; j <= '8'; j++)
 		{
-			for (k = j + 1; k <= 9; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
+				if (i != '7')
 				putchar(',');
 				putchar(' ');
 			}
