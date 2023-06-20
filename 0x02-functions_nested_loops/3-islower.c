cf09e0d064f113aa986424ab1f55d9bbba315f7c
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-20 14:32:54 -0700

    yo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,18 +4,8 @@
  */
 int _islower(int c)
 {
-	int a;
-
-	for (a = 97; a <= 122; a++)
-	{
-		if (a == c)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return 0;
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
