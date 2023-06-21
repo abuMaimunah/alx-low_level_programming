59160f0fd682594db5415d367c07075b29dd54e6
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-21 16:35:41 -0700

    yo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,11 @@ void print_to_98(int n)
 
 		for(i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 		}
 		printf("\n");
 	}
@@ -22,12 +26,16 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 		}
 		printf("\n");
 	}
 	else
 	{
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 }
