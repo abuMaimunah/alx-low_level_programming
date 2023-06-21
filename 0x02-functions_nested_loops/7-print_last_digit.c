b9e8f698f3e958bdeec6be3f0ea69ee990b787ce
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-21 13:09:37 -0700

    yo

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,17 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
-	
-	_putchar(ld);
-	return (ld);
+	int ld;
+
+	ld = n % 10;
+	if (ld < 0)
+	{
+		_putchar(-ld + '0');
+		return (-ld);
+	}
+	else
+	{
+		_putchar(ld + '0');
+		return (ld);
+	}
 }
