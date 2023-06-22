ed615b051c207032acf843a532dbfafc572edb05
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-22 14:46:31 -0700

    files are taking to the right directory

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test function for our positive_or_negative function
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = -20;
+	positve_or_negative(i);
+	return (0);
+}
