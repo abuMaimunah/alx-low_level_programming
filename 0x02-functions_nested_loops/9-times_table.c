d184d9b6037e24784a18d129c77ab40f7d1554d9
Muhammad Ibrahim <muhammadibrahim63118@gmail.com>
2023-06-21 17:51:14 -0700

    yo

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - Generate 9x time table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			_putchar(k);
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
