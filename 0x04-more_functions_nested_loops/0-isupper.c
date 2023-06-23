11df6882bc46266262e8db0f982ecc5bbab85423
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-23 00:55:10 -0700

    yo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - function
+ * @c: - parameter
+ * Return: 1 for true and o otherewise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
