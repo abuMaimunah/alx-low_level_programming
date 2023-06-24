250e33e1512a5b0bb6f354dcf45712965dd7a21d
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-24 13:55:41 -0700

    yo

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,12 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
+		_putchar('\n');
 		return;
 	}
-	for (j = 0; j < n; j++)
+	else
 	{
+		for (j = 0; j < n; j++);
 		_putchar('_');
 	}
 	_putchar('\n');
