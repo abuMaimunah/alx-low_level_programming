511819146c7228e5d95fc73db1c2d1deaf4056dd
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-23 23:47:32 -0700

    yo

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers with some format
+ */
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		int b;
+
+		for (b = 0; b <= 14; b++)
+		{
+			if (b <= 9)
+			{
+				_putchar(b + '0');
+			}
+			else
+			{
+				int tens;
+				int ones;
+
+				tens = b / 10 + '0';
+				ones = b % 10 + '0';
+				_putchar(tens);
+				_putchar(ones);
+			}
+		}
+		_putchar('\n');
+	}
+}
+	
