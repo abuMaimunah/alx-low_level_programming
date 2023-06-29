dd6e3eedb8a9be7d62161328da9ae9c331f73739
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-29 10:24:54 -0700

    Ptogtamming is an art

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Function that prints n number of elemenet
+ * @a: - integer array
+ * @n: - number of element to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n-1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
