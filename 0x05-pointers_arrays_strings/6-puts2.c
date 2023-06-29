3e088fc222f72bca1ed1e71994b777c58fc8369a
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-29 07:49:27 -0700

    yo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,15 @@
 #include "main.h"
+/**
+ * puts2 - Function definition
+ * @str: - my char pointer
+ */
 void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i = i + 2)
 	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
