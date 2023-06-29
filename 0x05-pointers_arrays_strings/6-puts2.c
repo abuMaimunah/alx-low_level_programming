18cf20cf38200ccee988f00eef69943041029722
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-29 07:36:37 -0700

    yo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,37 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-void puts2(char *str);
-int main(void)
-{
-	char *str;
-	
-	str = "0123456789";
-	puts2(str);
-	return (0);
-}
-
-
 void puts2(char *str)
 {
-	int len;
-	int count;
-	char newArr[100];
+	int i = 0;
 
-	len = 0;
-	count = 0;
-	while (*str)
+	for (; str[i]; i+=2)
 	{
-		len++;
-		str++;
-	//	printf("%d", len);
-	//	_putchar(len);
+		_putchar(str[i]);
 	}
-	while (count < len)
-	{
-		if (str[count] % 2 == 0)
-		{
-			newArr[count] = str[count];
-		}
-		printf("%c\n", newArr[count]);
-	}
+	_putchar('\n');
 }
