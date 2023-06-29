7fafb23bf5d4eea6b0acdabba9fcef12cda5cfe0
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-06-29 07:44:08 -0700

    Pointers are difficult

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,9 +3,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i]; i = i + 2)
+	for (; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
