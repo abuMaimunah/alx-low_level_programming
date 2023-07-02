4ab6777e60eed5c79e16eb93ac9dc47812fd687d
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-02 10:53:14 -0700

    I'm getting a big confident

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strcmp - The compare function
+ * @s1: The first string
+ * @s2: The second string
+ * Return: 0, 15 0r -15 is returned
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 < *s2)
+		{
+			return (-15);
+		}
+		else if (*s1 > *s2)
+		{
+			return (15);
+		}
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	if (*s1 > *s2)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
+	}
+}
