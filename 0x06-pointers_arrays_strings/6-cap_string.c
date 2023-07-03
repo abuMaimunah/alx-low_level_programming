6dea60000773b6e15b77be9faf69ab1359770448
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-03 00:51:38 -0700

    Programming na art i swear

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - Function that capitalize words based on some seperators
+ * @s: The char pointer
+ * Return: The starting position is returned
+ */
+char *cap_string(char *s)
+{
+	char *start = s;
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - ('a' - 'A');
+	}
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '\"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'))
+		{
+			s[i] = s[i] - ('a' - 'A');
+		}
+	}
+	return (start);
+}
