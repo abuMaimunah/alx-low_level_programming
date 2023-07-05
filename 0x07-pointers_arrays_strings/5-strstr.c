7a337d6e78b41fc2e62f96302588a50ea3bb9282
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-05 07:35:02 -0700

    Function that returns a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - A function that locates a substring
+ * @haystack: Char pointer to the string
+ * @needle: The substring
+ * Return: 0 success
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			for (; needle[j]; j++)
+			{
+				if (haystack[i + j] == needle[j])
+				{
+					continue;
+				}
+			}
+			return (&haystack[i]);
+		}
+	}
+	return (NULL);
+}
