6650f4d03f5ddd24d2c06b26d7f26919cc780652
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-05 08:48:14 -0700

    I added a break statement when a mismatch is found

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,7 +9,11 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
-		
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 	for (i = 0; haystack[i]; i++)
 	{
 		if (haystack[i] == needle[0])
