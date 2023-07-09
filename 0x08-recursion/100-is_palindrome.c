f26e26e072d0bdd6fd37082760e06e1f49291cb4
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-09 15:59:50 -0700

    A function that checks for palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,22 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 #include <string.h>
 /**
- * is_palindrome - A function that checks palindrome
+ * is_palindrome_helper - A function that checks palindrome
  * @s: The char array pointer
+ * @start: Start
+ * @end: End
+ * Return: 1 success
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	/* Base case: Empty string or single character is a palindrome */
+	if (start >= end)
+	{
+		return (1);
+	}
+	/* Check if the first and last characters match */
+	if (s[start] != s[end])
+	{
+		return (0); /* Not a palindrome */
+	}
+    /* Recursively check the substring without the first and last characters */
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+/**
+ * is_palindrome - CHeck for palindrome
+ * @s: Char array
  * Return: 1 success
  */
 int is_palindrome(char *s)
 {
 	int length = strlen(s);
-	int i, j;
 
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_palindrome_helper(s, 0, length - 1));
 }
