a7187a3b8c28d707e652f2f99a8d3486c458e729
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-09 16:21:35 -0700

    Compares two string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+/**
+ * wildcmp - Function that compares two string
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 success.
+ */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-    {
-        return 1;
-    }
-    if (*s2 == '*')
-    {
-        if (*s1 == '\0')
-        {
-            return wildcmp(s1, s2 + 1); 
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return 1;
 	}
-        else
-        {
-            return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
-        }
-    }
-    if (*s1 == *s2)
-    {
-        return wildcmp(s1 + 1, s2 + 1);
-    }
-
-    return 0;
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		{
+			return wildcmp(s1, s2 + 1);
+	}
+	else
+	{
+		return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+	}
+    	}
+	if (*s1 == *s2)
+	{
+	return wildcmp(s1 + 1, s2 + 1);
+	}
+	return 0;
 }
