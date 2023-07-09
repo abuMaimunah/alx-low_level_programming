1100646234ce5ab44fc606b6aea5f8460cf939a2
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-08 17:11:58 -0700

    A wrote a program that multiplies two numbers by accepting some command line arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument's count
+ * @argv: char array
+ * Return: 0 success 1 error
+ */
 int main(int argc, char *argv[])
 {
 	int a, b;
+	
 	if (argc < 3)
 	{
 		printf("%s\n", "Error");
