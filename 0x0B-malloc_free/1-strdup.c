80b266ed544efbd981b197e20f0eb034ede55eb8
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-10 09:33:53 -0700

    I used malloc to make a copy of a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - A function that returns a pointer to a newly all. space
+ * @str: The array
+ * Return: The newly allocated copy is returned
+ */
+char *_strdup(char *str)
+{
+	int len, i;
+	char *copy;
+
+	len = strlen(str);
+	copy = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		copy[i] = str[i];
+	}
+	return (copy);
+}
