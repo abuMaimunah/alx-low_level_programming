3b8446c21450be2fe4ebc0ccb9c47d81b7c15e8f
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-10 04:43:12 -0700

    A function that dynamically creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,11 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	unsigned int i;
 
-	if (size == 0)
+	ar = malloc(sizeof(char) * size);
+	if (s == NULL)
 	{
 		return (NULL);
 	}
-	ar = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		ar[i] = c;
