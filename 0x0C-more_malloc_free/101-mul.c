ae78e7cc59be106229ed7d305de66ffd3673a3ac
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-14 10:45:58 -0700

    A program that multiplies two positive numbers (command line arguments)

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument's count
+ * @argv: argument's value
+ * Return: 0 success;
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, num1, num2;
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(argv[i][j] >= '0') &&
+					!(argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+	return (0);
+}
