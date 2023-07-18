f29572af164402a8816adc127991b425fe02a404
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-18 14:44:56 -0700

    A god struct that organize some of its properties of different type

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,16 @@
+#ifndef HEADER
+#define HEADER
+/**
+ * struct dog - A dog struct that organize its name, age and owner
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
