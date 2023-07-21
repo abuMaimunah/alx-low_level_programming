a055675de94f832fdac2d2b10149265cdef1caba
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-21 03:12:43 -0700

    A function that frees dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,11 +10,11 @@ void free_dog(dog_t *d)
 	{
 		return;
 	}
-	if(d->name != NULL)
+	if (d->name != NULL)
 	{
 		free(d->name);
 	}
-	if(d->owner != NULL)
+	if (d->owner != NULL)
 	{
 		free(d->owner);
 	}
