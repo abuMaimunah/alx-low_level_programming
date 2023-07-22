94dfef40880d302ce70ea4bfecfb2641c2d1b031
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-22 15:44:40 -0700

    A function that executes a function give as a parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * array_iterator - function that executes a function
  * @array: a pointer to an int array
@@ -9,11 +10,20 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	size_t i, len = 0;
 
 	if (action == NULL)
 	{
 		exit(-1);
+	}
+	while (*array)
+	{
+		len++;
+		array++;
+	}
+	if (len != size)
+	{
+		exit(-2);
 	}
 	for (i = 0; i < size; i++)
 	{
