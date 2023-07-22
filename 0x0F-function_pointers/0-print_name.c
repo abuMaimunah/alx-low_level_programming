fa1533cb864f770364b3b5094e3ab71dcd633c53
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-07-22 15:13:29 -0700

    A funtion that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * print_name - A function that prints a name dynamically
  * @name: The char name
@@ -6,5 +8,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+	{
+		exit(98);
+	}
 	f(name);
 }
