08d62f8a21462c41e0a00da3480b5b65692e68a6
abuMaimunah <muhammadibrahim63118@gmail.com>
2023-08-11 23:38:07 -0700

    A program that displays the info contained in the ELF header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,11 +6,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <elf.h>
+/**
+ * pError - error message
+ * @message: message
+ */
 void pError(const char *message)
 {
 	fprintf(stderr, "Error: %s\n", message);
 	exit(98);
 }
+/**
+  * pElfHeaderInfo - header info
+  * @header: header
+  */
 void pElfHeaderInfo(const Elf64_Ehdr *header)
 {
 	int i;
@@ -20,14 +28,22 @@ void pElfHeaderInfo(const Elf64_Ehdr *header)
 	{
 		printf("%02x ", header->e_ident[i]);
 	}
-	printf("\nClass: %s\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-	printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little-endian" : "2's complement, big-endian");
+	printf("\nClass: %s\n", (header->e_ident[EI_CLASS] ==
+				ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("Data: %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+			"2's complement, little-endian" : "2's complement, big-endian");
 	printf("Version: %d\n", header->e_ident[EI_VERSION]);
 	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
 	printf("ABI Version %d\n", header->e_ident[EI_ABIVERSION]);
 	printf("Type: %d\n", header->e_type);
 	printf("Entry point address: 0x%lx\n", (unsigned long)header->e_entry);
 }
+/**
+  * main - entry point
+  * @argc: argument count
+  * @argv: argument value
+  * Return: success
+  */
 int main(int argc, char *argv[])
 {
 	const char *filename;
@@ -48,8 +64,10 @@ int main(int argc, char *argv[])
 	{
 		pError("Failed to read ELF header");
 	}
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || 
-			header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+			header.e_ident[EI_MAG1] != ELFMAG1 ||
+			header.e_ident[EI_MAG2] != ELFMAG2 ||
+			header.e_ident[EI_MAG3] != ELFMAG3)
 	{
 		pError("Not an ELF file");
 	}
