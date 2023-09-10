814bbc6f487682b081b3d5ffa46143985ac3309b
Ibrahim Muhammad <muhammadibrahim63118@gmail.com>
2023-09-10 04:41:42 -0700

    A program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,9 +7,9 @@
 #include <errno.h>
 
 #define BUFFER_SIZE 1024
-void print_error_and_exit(int code, const char *errorMessage, const char *file_name)
+void print_error_and_exit(int code, const char *errorMessage)
 {
-	dprintf(STDERR_FILENO, "Error: %s\n", errorMessage, file_name);
+	dprintf(STDERR_FILENO, "Error: %s\n", errorMessage);
 	exit(code);
 }
 int main(int argc, char *argv[])
@@ -24,20 +24,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		print_error_and_exit(97, "Usage: cp file_from file_to", "");
+		print_error_and_exit(97, "Usage: cp file_from file_to");
 	}
 	fileFrom = argv[1];
 	fileTo = argv[2];
 	fdFrom = open(fileFrom, O_RDONLY);
 	if (fdFrom == -1)
 	{
-		print_error_and_exit(98, "Can't read from file", fileFrom);
+		print_error_and_exit(98, "Can't read from file");
 	}
 	fdTo = open(fileTo, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IROTH);
 	if (fdTo == -1)
 	{
-		print_error_and_exit(99, "Can't write to file", fileTo);
+		print_error_and_exit(99, "Can't write to file");
 	}
 
 	while ((bytesRead = read(fdFrom, buffer, BUFFER_SIZE)) > 0)
@@ -45,20 +45,20 @@ int main(int argc, char *argv[])
 		bytesWritten = write(fdTo, buffer, bytesRead);
 		if (bytesWritten == -1)
 		{
-			print_error_and_exit(99, "Can't write to file", fileTo);
+			print_error_and_exit(99, "Can't write to file");
 		}
 	}
 	if (bytesRead == -1)
 	{
-		print_error_and_exit(99, "Can't write to file", fileTo);
+		print_error_and_exit(99, "Can't write to file");
 	}
 	if (close(fdFrom) == -1)
 	{
-		print_error_and_exit(100, "Can't close fd", fileFrom);
+		print_error_and_exit(100, "Can't close fd");
 	}
 	if (close(fdTo) == -1)
 	{
-		print_error_and_exit(100, "Can't close fd", fileTo);
+		print_error_and_exit(100, "Can't close fd");
 	}
 	return (0);
 }
